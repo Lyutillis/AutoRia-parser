402dc913bb20f6505128403a0962f887735e58d4
Lytillis <chemurchem@gmail.com>
2024-02-26 16:15:32 +0200

    Finalized threading+Playwright parser version

--- m_playwright_threads/__main__.py
+++ m_playwright_threads/__main__.py
@@ -4,7 +4,14 @@ from typing import List
 import threading
 import time
 from requests.exceptions import ChunkedEncodingError
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import (
+    sync_playwright,
+    Playwright,
+    Browser,
+    BrowserContext,
+    Page
+)
+from parsel import Selector
 
 from database.db_layer import PostgresDB, Car
 from parsers.parser import AutoriaParser, AutoriaParserV1, AutoriaParserV2
@@ -26,11 +33,15 @@ class AutoriaScraper:
 
     def __init__(self) -> None:
         self.results: List[Car] = []
-        self.pages = envs.PAGES
+        self.pages: int = envs.PAGES
 
         self.threads: List[threading.Thread] = []
-        self.db_is_busy = False
-        self.max_threads = 4
+        self.max_threads: int = 4
+
+        self.db_is_busy: bool = False
+        self.db_thread: threading.Thread = None
+
+    def start_db_thread(self) -> None:
         self.db_thread = threading.Thread(
             target=self.bulk_save,
             daemon=True,
@@ -48,7 +59,7 @@ class AutoriaScraper:
             self.db_is_busy = True
 
             with PostgresDB() as db:
-                results = []
+                results: list = []
                 for item in self.results[:]:
                     self.results.remove(item)
                     results.append(item)
@@ -59,75 +70,87 @@ class AutoriaScraper:
             if not thread.is_alive():
                 self.threads.remove(thread)
 
-    def accept_cookies(self, page):
+    def accept_cookies(self, page: Page) -> None:
         page.get_by_text("Розумію і дозволяю").click()
 
-    def scrape_list_page(self, page_number: int) -> None:
-        with sync_playwright() as pw:
-            browser = pw.chromium.launch(
-                headless=False,
-            )
-            context = browser.new_context()
-            page = context.new_page()
+    def run_thread(self, page_number: int) -> None:
+        playwright: Playwright = sync_playwright().start()
+        browser: Browser = playwright.chromium.launch(
+            headless=False,
+        )
+        context: BrowserContext = browser.new_context()
+        page: Page = context.new_page()
+        try:
+            self.scrape_list_page(page, page_number)
+        finally:
+            page.close()
+            context.close()
+            browser.close()
+            playwright.stop()
 
-            page.goto(urljoin(BASE_URL, f"?page={page_number}"))
-            content = page.content()
+    def scrape_list_page(self, page: Page, page_number: int) -> None:
+        page.goto(urljoin(BASE_URL, f"?page={page_number}"))
+        content = page.content()
 
-            self.accept_cookies(page)
+        if not AutoriaParser.check_list_page(content):
+            scraper_logger.warning("Reached last page. Terminating...")
+            raise EmptyPageException("Reached last page.")
 
-            if not AutoriaParser.check_list_page(content):
-                scraper_logger.warning("Reached last page. Terminating...")
-                raise EmptyPageException("Reached last page.")
+        self.accept_cookies(page)
 
-            urls = AutoriaParser.get_urls(content)
-            scraper_logger.info(f"Parsing page {page_number}")
-            for url in urls:
-                page.goto(url)
+        urls = AutoriaParser.get_urls(content)
 
-                if page.query_selector(
-                    "//*[contains(@class, 'phone_show_link')]"
-                ):
-                    page.locator(
-                        "(//*[contains(@class, 'phone_show_link')])[1]"
-                    ).click()
-                    time.sleep(0.3)
-                    content = page.content()
-                    parser = AutoriaParserV1(content, url)
-                else:
-                    content = page.content()
-                    from parsel import Selector
-                    selector = Selector(text=content)
-                    print(selector.xpath(
-                        "//div[@class='sellerInfoHiddenPhone']"
-                        "//button[@class='s1 conversion')]"
-                    ))
-                    # page.locator(
-                    #     "(//div[@class='sellerInfoHiddenPhone']"
-                    #     "//button[@class='s1 conversion')])[1]"
-                    # ).click()
-                    # time.sleep(0.3)
-                    # content = page.content()
-                    # parser = AutoriaParserV2(content, url)
+        scraper_logger.info(f"Parsing page {page_number}")
 
-                try:
-                    self.results.append(parser.parse_detail_page())
-                except (NoVinException, NoUsernameException, SoldException):
-                    continue
+        for url in urls:
+            page.goto(url)
+
+            if page.query_selector(
+                "//*[contains(@class, 'phone_show_link')]"
+            ):
+                page.locator(
+                    "(//*[contains(@class, 'phone_show_link')])[1]"
+                ).click()
+
+                time.sleep(0.3)
+
+                content = page.content()
+                parser = AutoriaParserV1(content, url)
+            else:
+                content = page.content()
+                selector = Selector(text=content)
+                print(url)
+                print(selector.xpath(
+                    "//div[@class='sellerInfoHiddenPhone']"
+                    "//button[@class='s1 conversion']"
+                ))
+                # page.locator(
+                #     "(//div[@class='sellerInfoHiddenPhone']"
+                #     "//button[@class='s1 conversion')])[1]"
+                # ).click()
+                # time.sleep(0.3)
+                # content = page.content()
+                # parser = AutoriaParserV2(content, url)
+
+            try:
+                self.results.append(parser.parse_detail_page())
+            except (NoVinException, NoUsernameException, SoldException):
+                continue
 
         scraper_logger.info(f"Finished parsing page {page_number}")
 
     def run(self) -> None:
-        current_page = 1
+        current_page: int = 1
 
         scraper_logger.info("Launched parser")
-
+        self.start_db_thread()
         try:
             while current_page <= self.pages:
                 self.clean_threads()
 
                 if len(self.threads) < self.max_threads:
-                    thread = threading.Thread(
-                        target=self.scrape_list_page,
+                    thread: threading.Thread = threading.Thread(
+                        target=self.run_thread,
                         args=(current_page,)
                     )
                     thread.start()
@@ -136,6 +159,7 @@ class AutoriaScraper:
                     continue
 
                 time.sleep(0.01)
+
         except EmptyPageException:
             pass
 
